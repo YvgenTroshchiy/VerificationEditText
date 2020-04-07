49403e8e44c9a44abc8531a965a7491fa93683e4
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2020-04-07 18:36:52 +0300

    initial commit

---
+++ verificationedittextlibrary/src/main/java/com/troshchiy/verificationedittextlibrary/VerificationEditText.kt
@@ -0,0 +1,15 @@
+package com.troshchiy.verificationedittextlibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.AttrRes
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class VerificationEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+}
