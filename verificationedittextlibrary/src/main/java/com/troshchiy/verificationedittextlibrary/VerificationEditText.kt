13227321e4d98b585e155cbfc9cf8c3ef01d7c35
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2020-04-08 19:07:51 +0300

    wip

--- verificationedittextlibrary/src/main/java/com/troshchiy/verificationedittextlibrary/VerificationEditText.kt
+++ verificationedittextlibrary/src/main/java/com/troshchiy/verificationedittextlibrary/VerificationEditText.kt
@@ -5,12 +5,22 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.util.AttributeSet
 import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
+
+fun colorStateListOf(@ColorInt color: Int): ColorStateList {
+    return ColorStateList.valueOf(color)
+}
+
+fun colorStateListOf(vararg mapping: Pair<IntArray, Int>): ColorStateList {
+    val (states, colors) = mapping.unzip()
+    return ColorStateList(states.toTypedArray(), colors.toIntArray())
+}
 
 class VerificationEditText @JvmOverloads constructor(
     context: Context,
@@ -20,6 +30,27 @@ class VerificationEditText @JvmOverloads constructor(
 
     private lateinit var textInputLayout: TextInputLayout
     private lateinit var textInputEditText: TextInputEditText
+
+    private val states = arrayOf(
+        /* enabled */ intArrayOf(android.R.attr.state_enabled),
+        /* disabled */ intArrayOf(-android.R.attr.state_enabled),
+        /* unchecked */ intArrayOf(-android.R.attr.state_checked),
+        /* pressed */ intArrayOf(android.R.attr.state_pressed)
+    )
+
+    private val colors = intArrayOf(
+        /* enabled */ Color.BLUE,
+        /* disabled */ Color.BLUE,
+        /* unchecked */ Color.BLUE,
+        /* pressed */ Color.BLUE
+    )
+
+    private val errorColor = ContextCompat.getColor(context, R.color.tv_error)
+    private val testColor = ContextCompat.getColor(context, R.color.test)
+
+    private val errorStateList = colorStateListOf(errorColor)
+
+    private val colorStateList = colorStateListOf(testColor)
 
     init {
         inflate(context, R.layout.verification_edittext_widget, this)
@@ -38,6 +69,8 @@ class VerificationEditText @JvmOverloads constructor(
     private fun setupViews() {
         textInputLayout = findViewById(R.id.textInputLayout)
 
+        textInputLayout.setErrorTextColor(errorStateList)
+
         textInputLayout.editText?.doOnTextChanged { text, start, count, after ->
             setError("")
         }
@@ -52,29 +85,6 @@ class VerificationEditText @JvmOverloads constructor(
     fun setSuccess() {
         setError("")
 
-        val states = arrayOf(
-            /* enabled */ intArrayOf(android.R.attr.state_enabled),
-            /* disabled */ intArrayOf(-android.R.attr.state_enabled),
-            /* unchecked */ intArrayOf(-android.R.attr.state_checked),
-            /* pressed */ intArrayOf(android.R.attr.state_pressed)
-        )
-
-//        val colors = intArrayOf(
-//            /* enabled */ Color.BLACK,
-//            /* disabled */ Color.RED,
-//            /* unchecked */ Color.GREEN,
-//            /* pressed */ Color.BLUE
-//        )
-
-        val colors = intArrayOf(
-            /* enabled */ Color.BLUE,
-            /* disabled */ Color.BLUE,
-            /* unchecked */ Color.BLUE,
-            /* pressed */ Color.BLUE
-        )
-
-        val testColor = ContextCompat.getColor(context, R.color.test)
-        val colorStateList = ColorStateList(states, colors)
 
 //        textInputLayout.boxStrokeColor = testColor // Selected stroke color
 //        textInputLayout.counterTextColor = colorStateList
