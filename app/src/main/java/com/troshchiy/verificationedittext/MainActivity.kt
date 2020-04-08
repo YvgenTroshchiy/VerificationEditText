16f3d422767c651a716f88b09eb72bd90e6b2a40
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2020-04-08 15:57:11 +0300

    Add btn_error, btn_success

--- app/src/main/java/com/troshchiy/verificationedittext/MainActivity.kt
+++ app/src/main/java/com/troshchiy/verificationedittext/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.troshchiy.verificationedittext
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_error.setOnClickListener {
+
+        }
+        btn_success.setOnClickListener {
+
+        }
     }
 }
--- verificationedittextlibrary/src/main/java/com/troshchiy/verificationedittextlibrary/VerificationEditText.kt
+++ verificationedittextlibrary/src/main/java/com/troshchiy/verificationedittextlibrary/VerificationEditText.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.annotation.AttrRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class VerificationEditText @JvmOverloads constructor(
@@ -12,8 +14,31 @@ class VerificationEditText @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    init {
+    private var textInputLayout: TextInputLayout? = null
+    private var textInputEditText: TextInputEditText? = null
 
+    init {
+        inflate(context, R.layout.verification_edittext_widget, this)
+
+        attrs?.let { initAttrs(it) }
+        setupViews()
+    }
+
+    private fun initAttrs(attrs: AttributeSet) {
+//        val a = context.obtainStyledAttributes(attrs, R.styleable.CenteredIconButton)
+//        a.recycle()
+    }
+
+    private fun setupViews() {
+        textInputLayout = findViewById(R.id.textInputLayout)
+        textInputEditText = findViewById(R.id.textInputEditText)
+    }
+
+    fun setSuccess() {
+
+    }
+
+    fun setError(message: String) {
 
     }
 }
